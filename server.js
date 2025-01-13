aecba80f5f48be4b4c69e406051c0150b2d70003
jefersonprimer <jefersonpriimer@gmail.com>
2025-01-13 15:07:29 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+// backend/server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 5000;
+
+// Carregar os dados JSON
+const animesData = require('./animes.json');
+const episodesData = require('./episodes.json');
+
+// Rota para retornar os animes
+app.get('/api/animes', (req, res) => {
+  res.json(animesData);
+});
+
+// Rota para retornar os episódios
+app.get('/api/episodes', (req, res) => {
+  res.json(episodesData);
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
