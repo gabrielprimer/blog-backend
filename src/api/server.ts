fd6376fc23ae4aa763a3d2e31830d0bfd2a47342
jefersonprimer <jefersonpriimer@gmail.com>
2025-01-13 15:47:44 -0300

    feat: put server.ts in api, and vercerl file create

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,13 +1,14 @@
-// src/server.ts
+// src/api/server.ts
 import express from 'express';
 import path from 'path';
 
+// Criação da instância do express
 const app = express();
 const port = 5000;
 
-// Carregar os dados JSON
-const animesData = require('./animes.json');
-const episodesData = require('./episodes.json');
+// Carregar os dados JSON com caminho correto
+const animesData = require('../animes.json'); // Corrigir o caminho
+const episodesData = require('../episodes.json'); // Corrigir o caminho
 
 // Rota para retornar os animes
 app.get('/api/animes', (req, res) => {
